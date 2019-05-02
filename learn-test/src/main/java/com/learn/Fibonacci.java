f23ba121b2209f727d1aca2b4d73f57cfebaeaa1
huzixia <huzixia@kanzhun.com>
2019-05-02 21:25:49 +0800

    learn-java

---
+++ learn-test/src/main/java/com/learn/Fibonacci.java
@@ -0,0 +1,240 @@
+package com.learn;
+
+
+import java.util.HashMap;
+
+/**
+ * 斐波那契数列实现方式
+ * 0,1,1,2,3,5,8,13
+ * 可以得出来结论
+ * F(0)=0
+ * F(1)=1
+ * F(2)=F(0)+F(1)=1
+ * F(3)=F(1)+F(2)=F(1)+( F(0)+F(1) ) = 2
+ * ......
+ * F(n)=F(n-2)+F(n-1)
+ * 细化分解，最终会回归到只有F(0)和F(1)的问题里面去，也就是等号右边全是F(0)和F(1)
+ *
+ *常用时间复杂度所耗费的时间从小到大依次是o(1）<o(log2^n)<o(n)<o(nlog2^n)<o(n^2)<o(n^3)<o(2^n)<o(n!)<o(n^n).
+ */
+public class Fibonacci {
+    /**
+     *第一种: 递归方式
+     * 时间复杂度为O(2^n), 空间复杂度就是树的高度 S(n)
+     */
+    public static long fibonacci_1 (int n ){
+        if(n<0) return -1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fibonacci_1(n-2)+fibonacci_1(n-1);
+    }
+
+
+    /**
+     *第二种: 顺序递推-基于变量
+     * 卸磨杀驴型 = 用完就给你覆盖上
+     * 时间复杂度：O(n)，空间复杂度：S(1)
+     */
+    public static long fibonacci_2(int n){
+//        if(n<0) throw new RuntimeException("输入参数小于1");
+        if(n<0) return -1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        long a=0;
+        long b=1;
+        long result=0;
+        for(int i=2;i<=n;i++){
+            result=a+b;
+            a=b;
+            b=result;
+        }
+        return result;
+    }
+
+    /**
+     * 第三种: 顺序递推-基于数组
+     * 如果需要的话，可以直接把整条斐波那契数列全带出来。直接返回数组就可以了
+     * 时间复杂度：O(n)，空间复杂度：S(1)
+     */
+    public static long fibonacci_3(int n){
+        if(n<0) return -1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        long[] arr=new long[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++) arr[i]=arr[i-2]+arr[i-1];
+        return arr[n];
+    }
+
+    /**
+     * 第四种: 尾递归方式
+     * 时间复杂度O(n)， 空间复杂度为S(n)—>优化S(1)
+     * 尾递归指函数体内最后一句执行为一个递归，该次递归执行后不再改变函数体
+     * 因此若编译器选择优化，只开辟一块函数体的空间
+     */
+    public static long fibonacci_4(int n,long num1,long num2){
+        if(n<0) return -1;
+        if(n==0) return 0;
+        if(n==1) return num1;
+        return fibonacci_4(n-1,num2,num1+num2);
+    }
+
+
+    /**
+     * 第五种：递归+HashMap缓存
+     * 时间复杂度为O（n），空间复杂度为S(n)
+     */
+    static HashMap<Integer,Long> map=new HashMap<Integer, Long>(); //缓存计算结果集
+    public static long fibonacci_5(int n){
+        if(n<0) return -1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(!map.containsKey(n)) map.put(n,fibonacci_5(n-2)+fibonacci_5(n-1));
+        return map.get(n);
+    }
+
+
+    /**
+     * 第六种：递归+数组缓存
+     * 时间复杂度为O（n），空间复杂度为S(n)
+     */
+    static long[] arr=new long[50+1];
+    public static long fibonacci_6(int n){
+        if(n<0) return -1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(arr[n]==0) arr[n]=fibonacci_6(n-2)+fibonacci_6(n-1);
+        return arr[n];
+    }
+
+    /**
+     *第七种: 公式解法, 等比数列推算
+     * 时间复杂度为O(1)
+     */
+    public static long fibonacci_7(int n ){
+        double result = 0;
+        double tmp=Math.sqrt(5.0);
+        result=(Math.pow((1+tmp)/2,n)-Math.pow((1-tmp)/2,n))/tmp;
+        return (long) result;
+    }
+
+    /**
+     *第八种: 矩阵解法(n-2次方)
+     * Fn=Fn-1+Fn-2
+     * [Fn-1]       [1 1]^ n-2    [1]
+     * |   |   =   |   |      *  | |
+     *[Fn-2]       [1 0]        [0]
+     */
+    static long[][] initMatrix={{1,1},{1,0}};
+    public static long fibonacci_8(int n){
+        if(n<0) return -1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        long[][] tmp=initMatrix;
+        for(int i=1;i<n-2;i++) tmp=matrixMulti(tmp,initMatrix);
+        return tmp[0][0]+tmp[1][0];
+    }
+
+    /**
+     * 两个二阶矩阵相乘
+     */
+//    public static long[][] matrixMulti(long[][] a ,long[][] b){
+//        long[][] tmp=new long[2][2];
+//        tmp[0][0]=a[0][0]*b[0][0]+a[0][1]*b[1][0];
+//        tmp[0][1]=a[0][0]*b[0][1]+a[0][1]*b[1][1];
+//        tmp[1][0]=a[1][0]*b[0][0]+a[1][1]*b[1][0];
+//        tmp[1][1]=a[1][0]*b[0][1]+a[1][1]*b[1][1];
+//        return tmp;
+//    }
+
+    /**
+     * 通用矩阵乘法
+     */
+    public static long[][] matrixMulti(long[][] a,long[][] b){
+        int rows=a.length;  //行数
+        int cols=b[0].length;  //列数
+        long[][] matrix=new long[rows][cols];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<b[0].length;j++) {
+                for (int k=0;k<a[i].length;k++) matrix[i][j]+=a[i][k] * b[k][j];  // a[i].length是a[i]的列数
+            }
+        }
+        return  matrix;
+    }
+
+    /**
+     * 第九种: 矩阵解法(n-2次方)+快速幂(位移法)
+     * 时间复杂度优化到O(logn), 空间复杂度：S(1)
+     */
+    static long[][] unitMatrix={{1,0},{0,1}}; //单位矩阵
+    public static long fibonacci_9(int n){
+        if(n<-1) return -1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        long[][] result=unitMatrix;
+        long[][] tmp=initMatrix;
+        int m=n-2;
+        while(m!=0){
+            if((m & 1)==1)  { //& 表示"按位与"，这里的"位"是指二进制位（bit）,&计算中，只要有一个是0就算成0
+                result=matrixMulti(tmp,result);
+                //System.out.println(m); //3 1  存储结果数据
+            }
+            tmp=matrixMulti(tmp,tmp);
+            m>>=1; //>>向右位移,先十进制转换成二进制, 再进行位移
+        }
+        return result[0][0]+result[1][0];
+    }
+
+    /**
+     * 第十种: 矩阵解法(n次方)+快速幂(二分法)
+     * [Fn+1   Fn]       [1 1]^ n
+     * |        |   =   |   |
+     *[Fn   Fn-1]       [1 0]
+     * 时间复杂度优化到O(logn), 空间复杂度：S(1)
+     */
+    public static long fibonacci_10(int n ){
+        if(n<0) return -1;
+        if(n==0) return 0;
+        long[][] result=power_dich(n);
+        return result[0][1];
+    }
+
+    public static long[][] power_dich(int n){
+        if(n==1) return initMatrix; //此行不能少
+        if(n%2 == 0) {//偶数
+            long[][] matrix=power_dich(n/2);
+            return matrixMulti(matrix,matrix);
+        }else{//奇数
+            long[][] matrix=power_dich((n-1)/2);
+            return matrixMulti(initMatrix,matrixMulti(matrix,matrix));
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        long begin = System.currentTimeMillis(); // 这段代码放在程序执行前
+//        long result=new Fibonacci().fibonacci_1(50); //59103毫秒
+//        long result=new Fibonacci().fibonacci_2(50); //16毫秒
+//        long result=new Fibonacci().fibonacci_3(50); //1毫秒
+//        long result=new Fibonacci().fibonacci_4(50,1,1); //0毫秒
+//        long result=new Fibonacci().fibonacci_5(50); //2毫秒
+//        long result=new Fibonacci().fibonacci_6(50); //1毫秒  12586269025
+        long result=new Fibonacci().fibonacci_7(49); //0毫秒
+//        long result=new Fibonacci().fibonacci_8(50); //0毫秒
+//        long result=new Fibonacci().fibonacci_9(50); //0毫秒
+//        long result=new Fibonacci().fibonacci_10(50); //0毫秒
+        System.out.println(result);
+        long end = System.currentTimeMillis() - begin; // 这段代码放在程序执行后
+        System.out.println("耗时：" + end + "毫秒");
+
+//        long[][] b={{1,1,2},{1,0},{1,1,0}};
+//        System.out.println(b[0].length);//3
+//        System.out.println(b[1].length);//2
+//        System.out.println(b[2].length);//3
+
+
+    }
+
+}
---
+++ learn-test/src/main/java/com/learn/Sort.java
@@ -0,0 +1,153 @@
+package com.learn;
+
+/**
+ * @Author: Huzixa
+ * @Date: 2019/5/2 15:02
+ */
+public class Sort {
+    /**
+     * 冒泡排序, 实现数组从小到大的排序
+     * 冒泡排序是一种稳定的排序方法, 平均时间复杂度为O(n^2)
+     */
+    public static int[] bubble_sort(int[] arr){
+        for (int i=0;i<arr.length-1;i++) { //外层循环控制排序趟数, arr.length-1指数组的最大索引值
+            for(int j=0;j<arr.length-1-i;j++){ //内层循环控制每一趟排序多少次
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * 快速排序,实现数组从小到大的排序
+     * 快速排序是不稳定的排序, 时间复杂度为O(nlogn)
+     */
+    public static int[] quick_sort(int[] arr,int low,int high){
+        if(low>high) return arr;
+        int i=low;
+        int j=high;
+        int key=arr[low];  //固定的切分方式,key为基准位
+        while(i<j){
+            while(arr[j]>=key && j>i) j--; //从后半部分向前扫描,如果列表右边的数,比基准数大或相等,则前移一位直到有比基准数小的数出现
+            while(arr[i]<=key && j>i) i++; //从前半部分向后扫描,如果列表左边的数,比基准数小或相等,则前移一位直到有比基准数大的数出现
+            //如果满足条件则交换ij数字
+            if(i<j){
+                int tmp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=tmp;
+            }
+        }
+        //最后将基准位和(low和high)相等位置的数字交换
+        arr[low]=arr[i];
+        arr[i]=key;
+        quick_sort(arr,low,j-1); //递归调用左半数组
+        quick_sort(arr,j+1,high); //递归调用右半数组
+        return arr;
+    }
+
+
+    /**
+     * 对给定的n个数的序列，返回序列中的最大和最小的数
+     * 最简单的方法: 逐个比较，返回最大的数和最小的数。此时需要进行2n次的比较。
+     */
+    public static int getMax_1(int[] arr){
+        int max=arr[0];
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]>max) max=arr[i];
+        }
+        return max;
+    }
+
+    public static int getMin_1(int[] arr){
+        int min=arr[0];
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]<min) min=arr[i];
+        }
+        return min;
+    }
+
+    /**
+     * 对给定的n个数的序列，返回序列中的最大和最小的数
+     * 先对各数据两两比较，将大的放在一起，小的放在一起，然后从大数集合中找最大值，从小数集合中找最小值。此时共需3n/2=1.5n次的比较。
+     */
+    public static int[] exchange(int[] arr){
+        for(int i=0;i<=(arr.length-1)/2;i++){
+            if(arr[i]>arr[arr.length-1-i]){
+                int tmp=arr[i];
+                arr[i]=arr[arr.length-1-i];
+                arr[arr.length-1-i]= tmp;
+            }
+        }
+        return arr;
+    }
+
+    public static int getMax_2(int[] arr){
+        int max=arr[arr.length-1];
+        for(int i=arr.length-1;i>=(arr.length-1)/2;i--) {
+            if (max < arr[i]) max = arr[i];
+        }
+        return max;
+    }
+
+    public static int getMin_2(int[] arr){
+        int min=arr[0];
+        for(int i=0;i<=(arr.length-1)/2;i++){
+            if (min>arr[i]) min=arr[i];
+        }
+        return min;
+    }
+
+
+    /**
+     * 对给定的n个数的序列，返回序列中的最大和最小的数
+     * 前两个数比较,大的为最大值, 小的为最小值, 用掉一次比较
+     * 后面n-2个数, 每两个比较, 大的同最大值比较, 小的同最小值比较, 3*(n-2)/2次比较, 共(1.5n-2)次比较。
+     */
+    public static void getMaxMin(int[] arr){
+        int max=0;
+        int min=0;
+        if(arr[0]>arr[1]) {
+            max=arr[0];min=arr[1];
+        }else{
+            max=arr[1];min=arr[0];
+        }
+        for(int i=2;i<=arr.length-1;i++){
+            if(arr[i]>arr[arr.length-1-i]){
+                if(arr[i]>max) max=arr[i];
+                if(arr[arr.length-1-i]<min) min=arr[arr.length-1-i];
+            }else{
+                if(arr[arr.length-1-i]>max) max=arr[arr.length-1-i];
+                if(arr[i]<min) min=arr[i];
+            }
+        }
+        System.out.println("最大值是: "+max);
+        System.out.println("最小值是: "+min);
+    }
+
+
+    public static void main(String[] args) {
+        long begin = System.currentTimeMillis(); // 这段代码放在程序执行前
+        int[] arr={22,1,34,2,0,61,5,7,99};
+//        System.out.println(getMax_1(arr));
+//        System.out.println(getMin_1(arr));
+
+        arr=exchange(arr);
+        System.out.println(getMax_2(arr));
+        System.out.println(getMin_2(arr));
+
+//        arr=bubble_sort(arr);
+//        arr=quick_sort(arr,0,arr.length-1);
+//        for (int i=0;i<=arr.length-1;i++) System.out.println(arr[i]);
+//        getMaxMin(arr);
+        long end = System.currentTimeMillis() - begin; // 这段代码放在程序执行后
+        System.out.println("耗时：" + end + "毫秒");
+
+    }
+
+
+
+}
